8020eadb0c562992abcf0cbfcb779d3097ebd274
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2022-12-24 16:28:21 -0500

    clear react template

--- my-playground/my-playground/src/App.js
+++ my-playground/my-playground/src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function card(props){
   return <div class="card-box"><h1>My Name Is {props.name}</h1></div>
 
-export
+}
+
+export default './App'
