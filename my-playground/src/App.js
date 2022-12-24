0d027a5a8bbead984b9d57298f34d2c889d1463b
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2022-12-24 11:34:12 -0500

    just playing around

--- my-playground/src/App.js
+++ my-playground/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="/danny-profile-pic.jpg" className="App-logo" alt="danielle pic"/>
       </header>
+      <div>
+        
+      </div>
     </div>
   );
+
+  
 }
+
+
 
 export default App;
