5f4b91e3ece6951041315eeb2e4c35bc8cf036b6
RMarmolejo90 <109112094+RMarmolejo90@users.noreply.github.com>
2023-01-06 05:47:04 -0500

    cleaned for tesla clone website

--- monster/src/App.js
+++ monster/src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import Monster from './monster';
-import logo from './logo512.png';
-import Contacts from './contacts';
-import Joke from './joke';
+
 
 
 function App() {
   return (
     <div className="App">
-      <Monster/>
+      
     </div> 
   );
 }
